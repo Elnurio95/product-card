fe2758b12745a01e512f2d028d3910bf751048a6
Elnur <136242453+Elnurio95@users.noreply.github.com>
2025-12-05 17:11:47 +0300

    [Homework №8]: start of tasks

---
+++ product-cards.js
@@ -0,0 +1,62 @@
+const productCardsArray = [
+    {
+        purpose: 'для нормальной кожи', 
+        name: 'Увлажняющий мусс',
+        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+        compoundone: 'активные натуральные комплексы',
+        compoundtwo: 'витамины С, А, РР, В И Е', 
+        compoundthree: 'солнцезащитные компоненты', 
+        cost: '2 750', 
+    },
+    {
+        purpose: 'для нормальной кожи', 
+        name: 'Увлажняющий мусс',
+        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+        compoundone: 'активные натуральные комплексы',
+        compoundtwo: 'витамины С, А, РР, В И Е', 
+        compoundthree: 'солнцезащитные компоненты', 
+        cost: '2 750', 
+    },
+    {
+        purpose: 'для нормальной кожи', 
+        name: 'Увлажняющий мусс',
+        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+        compoundone: 'активные натуральные комплексы',
+        compoundtwo: 'витамины С, А, РР, В И Е', 
+        compoundthree: 'солнцезащитные компоненты', 
+        cost: '2 750', 
+    },    
+    {
+        purpose: 'для нормальной кожи', 
+        name: 'Увлажняющий мусс',
+        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+        compoundone: 'активные натуральные комплексы',
+        compoundtwo: 'витамины С, А, РР, В И Е', 
+        compoundthree: 'солнцезащитные компоненты', 
+        cost: '2 750', 
+    },
+    {
+        purpose: 'для нормальной кожи', 
+        name: 'Увлажняющий мусс',
+        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+        compoundone: 'активные натуральные комплексы',
+        compoundtwo: 'витамины С, А, РР, В И Е', 
+        compoundthree: 'солнцезащитные компоненты', 
+        cost: '2 750 ', 
+    },
+]
+
+const productsList = document.getElementById('productsList');
+const productTemplate = document.getElementById('product'); 
+
+productCardsArray.forEach(productCard => {
+    const productCardClone = productTemplate.content.cloneNode(true); 
+    productCardClone.querySelector('.purpose').textContent = productCard.purpose;
+    productCardClone.querySelector('.name').textContent = productCard.name;
+    productCardClone.querySelector('.description').textContent = productCard.description;
+    productCardClone.querySelector('.compoundone').textContent = productCard.compoundone;
+    productCardClone.querySelector('.compoundtwo').textContent = productCard.compoundtwo;
+    productCardClone.querySelector('.compoundthree').textContent = productCard.compoundthree;
+    productCardClone.querySelector('.cost').textContent = `${productCard.cost} ₽`;
+    productsList.appendChild(productCardClone); 
+})
