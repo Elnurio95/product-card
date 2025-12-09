57fe882c1b08909b25adc40912879c699aec175a
Elnur <136242453+Elnurio95@users.noreply.github.com>
2025-12-09 14:50:45 +0300

    [Homework-8]: changed names and functions

--- homework-8.js
+++ homework-8.js
@@ -1,23 +1,23 @@
-/*Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой*/
 import { productCardsArray } from "./product-cards";
+/*Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой*/
 
-function productNamesArr() {
+function getProductNamesArr() {
     productCardsArray.reduce((acc, curr) => {
     return acc + curr.name; 
 }); 
 };
 
-console.log(productNamesArr); 
+getProductNamesArr();  
 
 /* Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание */
-function productDesc() {
+function getProductDesc() {
     return productCardsArray.reduce((acc, curr) => {
         acc.push({ [curr.name]: curr.description });
         return acc;  
     }, []); 
 }
 
-productDesc(); 
+getProductDesc(); 
 
 /* Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом, мол "Сколько карточек отобразить? От 1 до 5" и в зависимости от результата - будет выводить это количество. Должна быть защита от введенных других значений (имеется ввиду проверка if). */
 
--- product-cards.js
+++ product-cards.js
@@ -1,48 +1,43 @@
 export const productCardsArray = [
     {
-        purpose: 'для нормальной кожи', 
+        cardImg: './img/moist-musse.png',
+        skinType: 'для нормальной кожи', 
         name: 'Увлажняющий мусс',
         description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-        compoundone: 'активные натуральные комплексы',
-        compoundtwo: 'витамины С, А, РР, В И Е', 
-        compoundthree: 'солнцезащитные компоненты', 
-        cost: '2 750', 
+        compound: ['активные натуральные комплексы', 'витамины С, А, РР, В И Е', 'солнцезащитные компоненты'],
+        cost: 2750, 
     },
     {
-        purpose: 'для нормальной кожи', 
-        name: 'Увлажняющий мусс',
-        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-        compoundone: 'активные натуральные комплексы',
-        compoundtwo: 'витамины С, А, РР, В И Е', 
-        compoundthree: 'солнцезащитные компоненты', 
-        cost: '2 750', 
+        cardImg: './img/moist-mask.png',
+        skinType: 'для нормальной кожи', 
+        name: 'Увлажняющая маска',
+        description: 'Способствует удерживанию влаги в верхних слоях кожи.',
+        compound: ['воски', 'минералы', 'масла'],
+        cost: 3500, 
     },
     {
-        purpose: 'для нормальной кожи', 
-        name: 'Увлажняющий мусс',
-        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-        compoundone: 'активные натуральные комплексы',
-        compoundtwo: 'витамины С, А, РР, В И Е', 
-        compoundthree: 'солнцезащитные компоненты', 
-        cost: '2 750', 
+        cardImg: './img/cleansing-gel.png',
+        skinType: 'для нормальной кожи', 
+        name: 'Гель для умывания',
+        description: 'Интенсивно очищает, не повреждает защитный барьер кожи.',
+        compound: ['минералы', 'витамины С, А, РР, В И Е', 'солнцезащитные компоненты'],
+        cost: 1650, 
     },    
     {
-        purpose: 'для нормальной кожи', 
-        name: 'Увлажняющий мусс',
-        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-        compoundone: 'активные натуральные комплексы',
-        compoundtwo: 'витамины С, А, РР, В И Е', 
-        compoundthree: 'солнцезащитные компоненты', 
-        cost: '2 750', 
+        cardImg: './img/gift-set-1.png',
+        skinType: 'для нормальной кожи', 
+        name: 'Подарочный набор №1',
+        description: 'Набор, состоящий из увлажняющего крема и маски.',
+        compoundone: ['воски', 'минералы', 'масла'],
+        cost: 4750, 
     },
     {
-        purpose: 'для нормальной кожи', 
-        name: 'Увлажняющий мусс',
-        description: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-        compoundone: 'активные натуральные комплексы',
-        compoundtwo: 'витамины С, А, РР, В И Е', 
-        compoundthree: 'солнцезащитные компоненты', 
-        cost: '2 750 ', 
+        cardImg: './img/gift-set-5.png', 
+        skinType: 'для нормальной кожи', 
+        name: 'Подарочный набор №5',
+        description: 'Весь набор средств Invisible symphony, крем, маска, мусс и гель для',
+        compound: ['воски', 'минералы', 'масла'],
+        cost: 7520, 
     },
 ]
 
@@ -51,13 +46,12 @@ const productTemplate = document.getElementById('product');
 
 productCardsArray.forEach(productCard => {
     const productCardClone = productTemplate.content.cloneNode(true); 
-    productCardClone.querySelector('.purpose').textContent = productCard.purpose;
+    productCardClone.querySelector('.cardImg').src = productCard.cardImg; 
+    productCardClone.querySelector('.skinType').textContent = productCard.skinType;
     productCardClone.querySelector('.name').textContent = productCard.name;
-    productCardClone.querySelector('.description').textContent = productCardsArray.description;
-    productCardClone.querySelector('.compoundone').textContent = productCardsArray.compoundone;
-    productCardClone.querySelector('.compoundtwo').textContent = productCardsArray.compoundtwo;
-    productCardClone.querySelector('.compoundthree').textContent = productCardsArray.compoundthree;
-    productCardClone.querySelector('.cost').textContent = `${productCardsArray.cost} ₽`;
+    productCardClone.querySelector('.description').textContent = productCard.description;
+    productCardClone.querySelector('.compound').textContent = productCard.compound.join(', ');
+    productCardClone.querySelector('.cost').textContent = `${productCard.cost} ₽`;  
     productsList.appendChild(productCardClone); 
 })
 
